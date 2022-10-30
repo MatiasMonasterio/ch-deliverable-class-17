56081995300b8e45e896f4528c910ecb7f4f9595
MatiasMonasterio <matias_monasterio@outlook.com>
2022-10-30 14:49:18 -0300

    fix: import env from process
    Signed-off-by: MatiasMonasterio <matias_monasterio@outlook.com>

--- config/arg.js
+++ config/arg.js
@@ -1,7 +1,7 @@
 const yargs = require("yargs/yargs");
 const args = yargs(process.argv.slice(2));
 
-const PORT = precess.env.PORT || 8080; // TO DEPLOY ON HEROKU
+const PORT = process.env.PORT || 8080; // TO DEPLOY ON HEROKU
 const MODE = args.argv.MODE === "CLUSTER" ? "CLUSTER" : "FORK";
 
 module.exports = { PORT, MODE };
